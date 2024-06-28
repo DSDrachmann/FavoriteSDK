b03b8a4cb1f387ea97728688d0376b71c27dfa75
Dan <128813803+DSDrachmann@users.noreply.github.com>
2024-06-28 14:01:49 +0200

    Update FavoriteDao.kt

--- Favorite/src/main/java/com/Dandd/favorite/internal/database/FavoriteDao.kt
+++ Favorite/src/main/java/com/Dandd/favorite/internal/database/FavoriteDao.kt
@@ -5,18 +5,19 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import com.Dandd.favorite.internal.database.model.FavoriteEntity
+import com.Dandd.favorite.domain.model.FavoriteEntity
 
 
 //this is your interface, the interface that decides how you interact with the database
+//The FavoriteRoomDatabase inherits this
 @Dao
 interface FavoriteDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun setFavorite(favorite: FavoriteEntity)
+    suspend fun setFavorite(favorite: FavoriteEntity): Long
 
     @Delete
-    suspend fun removeFavorite(favorite: FavoriteEntity)
+    suspend fun removeFavorite(favorite: FavoriteEntity): Int
 
     @Query("Select * FROM favoriteEntity WHERE accountName = :inputAccountName AND entityType = :inputEntityType")
     suspend fun getFavorites(inputAccountName: String, inputEntityType: String): List<FavoriteEntity>
